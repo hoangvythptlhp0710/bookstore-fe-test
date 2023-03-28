a2b2f04b64e322b2cdd40af75818cf7ede0df7ee
nhunganh4502 <118982226+nhunganh4502@users.noreply.github.com>
2023-03-28 21:23:31 +0700

    Update Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -38,23 +38,23 @@ export default class Login extends Component {
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h3>Login</h3>
-                <div className="mb-3">
-                    <label>Username</label>
-                    <input type="text" id='username' className="form-control" placeholder="Enter username"
-                        onChange={this.handleChange}/>
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <input type="password" id='password' className="form-control" placeholder="Enter password"
-                        onChange={this.handleChange}/>
-                </div>
-                <div className="d-grid">
-                    <button type="submit" className="btn" variant="outline-dark">Submit</button>
-                </div>
-            </form>
-        )
-    }
+    return (
+        <form onSubmit={this.handleSubmit}>
+            <h3>Login</h3>
+            <div className="mb-3">
+                <label>Username</label>
+                <input type="text" id='username' className="form-control" placeholder="Enter username"
+                    onChange={this.handleChange}/>
+            </div>
+            <div className="mb-3">
+                <label>Password</label>
+                <input type="password" id='password' className="form-control" placeholder="Enter password"
+                    onChange={this.handleChange}/>
+            </div>
+            <div className="d-grid">
+                <button type="submit" className="btn" variant="outline-dark">Submit</button>
+            </div>
+        </form>
+    )
+}
 }
