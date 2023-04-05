c0b7e3c8d3d614a4b866f2b9d67473c77d8d7ccb
nhunganh4502 <nhunganh4502@gail.com>
2023-04-05 10:23:44 +0700

    get product by category

---
+++ src/components/products/ProductsByCategory.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react'
+import axios from "axios";
+import withRouter from './WithRouter';
+
+class ProductsByCategory extends Component {
+    state = {
+        products: [],
+    }
+
+    url = "http://localhost:8080/"
+
+    componentDidMount() {
+        const category = this.props.params.category;
+        const page = this.props.params.page;
+        this.fetchProducts(category, page);
+    }
+
+    fetchProducts(category, page) {
+        axios.get("http://localhost:8080/product/" + category + "/" + page)
+        .then((res) => {
+            this.setState({products: res.data.content})
+        })
+        .catch((error) => {
+
+        })
+    }
+
+    render() {
+        if (this.state.products.length !== 0) {
+            return (
+                <section className="container">
+                    <div className='carousel'>
+                        <img src="/images/book2.jpg" alt='book2'/>
+                    </div>
+                    <hr></hr>
+                    <div className="wrapper">
+                        <div className="inner">
+                            <div className="grid mg-left-10">
+                                <div className="products">
+                                    <div className="box">
+                                        {
+                                            this.state.products.map(product => (
+                                                <div className="item text-center" key={product.id}>
+                                                    <a href={"/product/" + product.id} className="product-item">
+                                                        <div className="product-img">
+                                                            <img className="lazy-load"
+                                                                 src={product.images[0]}
+                                                                 data-src={product.images}
+                                                                 alt={product.name}/>
+                                                            <div className="sale-off">-20%</div>
+                                                        </div>
+                                                        <div className="product-title">{product.name}</div>
+                                                        <div className="product-price">
+                                                            <span
+                                                                className="current-price">{product.price.toFixed(2)}$</span>
+                                                            <span
+                                                                className="original-price"><s>{(product.price * 1.2).toFixed(2)}$</s></span>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            )
+        }
+    }
+}
+
+export default withRouter(ProductsByCategory);
